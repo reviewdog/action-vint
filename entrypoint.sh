bb5068bb325c7d7eacff4cc9ed35f87438a7b38a
haya14busa <haya14busa@gmail.com>
2019-12-04 16:47:11 +0000

    fix default reporter value

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-vint . | reviewdog -efm="%f:%l:%c: %m" -name="vint" -reporter="${INPUT_REPORTER:-'github-pr-check'}" -level="${INPUT_LEVEL}"
+vint . | reviewdog -efm="%f:%l:%c: %m" -name="vint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
